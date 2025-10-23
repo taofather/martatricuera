14c509a13ea8f46ffe4f089ce7b228af365b77dc
Jordi Caballero <jordi.caballero@consentio.co>
2025-10-23 12:04:49 +0200

    first commit with _site output

---
+++ _site/js/custom.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+
+    /*
+      Youtube lazy loader: checks js-lazyYT elements
+    */
+    $('.js-lazyYT').lazyYT({
+      loading_text: '',
+      default_ratio: '16:9'
+    });
+});
